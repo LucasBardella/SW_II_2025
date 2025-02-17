259d530bb5ed9ca85e0a7662759dd60fed14753f
LucasBardella <lucasss.bardella.f@gmail.com>
2025-02-17 11:07:25 -0300

    exercicios

--- Aula2/exercicios.php
+++ Aula2/exercicios.php
@@ -109,12 +109,13 @@ echo "<br>";
 
 echo "Exercicio 8" . "<br>";
 
-echo "Digite um número: ";
-$numero = trim(fgets(STDIN));
+// Recebe um número do usuário
+$numero = 3;
 
+// Exibe a tabuada do número de 1 a 10
 echo "Tabuada de $numero:\n";
 for ($i = 1; $i <= 10; $i++) {
-    echo "$numero x $i = " . ($numero * $i) . "\n";
+    echo "$numero x $i = " . ($numero * $i) . "\n" . "<br>";
 }
 
 ?>
