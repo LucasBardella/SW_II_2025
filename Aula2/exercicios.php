1ef23e00e78fc0e5139bc1d61e2f527bcfaa0dd8
LucasBardella <lucasss.bardella.f@gmail.com>
2025-02-17 11:04:51 -0300

    exercicios

--- Aula2/exercicios.php
+++ Aula2/exercicios.php
@@ -35,9 +35,86 @@ echo "Exercicio 3" . "<br>";
 $numero = 4;
 
 if ($numero % 2 == 0) {
-    echo "O número $numero é par.";
+    echo "O número $numero é par." . "<br>";
 } else {
-    echo "O número $numero é ímpar.";
+    echo "O número $numero é ímpar." . "<br>";
+}
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+echo "Exercicio 4" . "<br>";
+
+$opcao = 2;
+switch ($opcao) {
+    case 1:
+        echo "Home escolhida";
+        break;
+    case 2:
+        echo "Menu escolhida";
+        break;
+    case 3:
+        echo "Contato escolhido";
+        break;
+    default:
+        echo "Opção inválida";
+} 
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+echo "Exercicio 5" . "<br>";
+
+$contador = 0;
+while ($contador < 10) {
+    echo "Número: $contador " . "<br>";
+    $contador++;
+}
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+echo "Exercicio 6" . "<br>";
+
+$soma = 0;
+
+for ($i = 1; $i <= 50; $i++) {
+    $soma += $i;
+}
+
+echo "A soma dos números de 1 a 50 é: " . $soma;
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+echo "Exercicio 7" . "<br>";
+
+$nomes = ["Ana", "Bruno", "Carlos", "Lucas", "Anderson", "Luana", "Vinicius", "Raphaela"]; 
+foreach ($nomes as $nome) {
+    echo "Nome: $nome " . "<br>";
+}
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+echo "Exercicio 8" . "<br>";
+
+echo "Digite um número: ";
+$numero = trim(fgets(STDIN));
+
+echo "Tabuada de $numero:\n";
+for ($i = 1; $i <= 10; $i++) {
+    echo "$numero x $i = " . ($numero * $i) . "\n";
 }
 
 ?>
