a4f665b3f5973b9f1af125d6f38fcdd6feecdc4d
LucasBardella <lucasss.bardella.f@gmail.com>
2025-02-17 09:43:50 -0300

    aula2

--- Aula1/calculamedia.php
+++ Aula1/calculamedia.php
@@ -1,7 +1,8 @@
 <?php
-$nota1 = 7
-$nota2 = 8
-$nota3 = 8
-$media = ($nota1 + $nota2 + $nota3) / 3
+$nota1 = 7;
+$nota2 = 8;
+$nota3 = 8;
+$media = ($nota1 + $nota2 + $nota3) / 3;
 
-echo "A sua média foi: " . $media
+echo "A sua média foi: " . $media;
+?>
--- Aula1/listapares.php
+++ Aula1/listapares.php
@@ -1,11 +1,12 @@
 <?php
-$pares
+$pares = ""; // Inicializando a variável para armazenar os números pares.
 
 for ($i = 1; $i <= 20; $i++) {
   if ($i % 2 == 0) {
-  $pares = $i
+    $pares .= $i . " "; // Concatenando os números pares à variável $pares
   }
 }
 
-echo "Número: " . $pares . "<br>";
+echo "Números pares: <br>" . $pares . "<br>";
 ?>
+
