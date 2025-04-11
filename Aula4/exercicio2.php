6f1c52930ebe76b5b8ff1d1a8b4c617122ed8975
Lucas Bardella <124801180+LucasBardella@users.noreply.github.com>
2025-04-11 11:11:48 -0300

    exercicio2.php

--- Aula4/exercicio2.php
+++ Aula4/exercicio2.php
@@ -1,7 +1,5 @@
 <?php
-
 $numeros = [5, 8, 12, 3, 7, 9, 15, 4, 6, 10];
-
 $soma = 0;
 
 foreach ($numeros as $numero) {
@@ -9,6 +7,4 @@ foreach ($numeros as $numero) {
 }
 
 echo "A soma é: $soma";
-
-
 ?>
